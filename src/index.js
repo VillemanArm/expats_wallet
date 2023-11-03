9bc8ad877a72de302ffcee025f393c3f2380faf8
Villeman <villeman.arm@gmail.com>
2023-11-03 14:40:26 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/base.sass';
+import Header from './components/Header'
+// import Converter from '/components/Converter'
+// import History from '/components/History'
+import Footer from './components/Footer'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    {/* <main>
+      <Converter />
+      <History />
+    </main> */}
+    <Footer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
