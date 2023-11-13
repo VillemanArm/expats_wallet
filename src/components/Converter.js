864b46ec79c1ba8b99b5d2a95c74f200e5f32453
Villeman <villeman.arm@gmail.com>
2023-11-13 12:44:20 +0300

    feat: getRates function to Converter

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,11 +1,40 @@
 import React from "react";
+import axios from "axios";
 import { ImPlus, ImArrowRight, ImCross } from "react-icons/im"
 import CurrencyBlock from "./CurrencyBlock";
 
+// const digits = /[^\d\.]/g
+
+
 class Converter extends React.Component {
+    constructor(props) {
+        super(props)
+
+
+
+        this.state = {
+            rates: ''
+        }
+    }
+
+
+    getRates() {
+        const base = 'USD'
+        const currencies = 'RUB,EUR,USD,AMD,GEL'
+        const resolution = '1m'
+        const url = `https://api.fxratesapi.com/latest?base=${base}&currencies=${currencies}&resolution=${resolution}&amount=1&places=6&format=json`
+
+        axios.get(url)
+            .then(res => {
+                console.log(res.data.rates)
+            })
+            .catch(err => console.log(err))
+
+    }
+
     render() {
         return (
-            <div className="converter">
+            <div className="converter" onLoad={this.getRates()}>
                 <div>
                     <div className="converter__head">
                         <h1>Converter</h1>
@@ -21,8 +50,8 @@ class Converter extends React.Component {
                     <select className="converter__target-currency">
 
                         <option value="none" selected></option>
-                        <option value="RUB" selected>RUB</option>
-                        <option value="USD" selected>USD</option>
+                        <option value="RUB">RUB</option>
+                        <option value="USD" >USD</option>
                     </select>
                     <button className="converter__calculator-button"> <ImArrowRight /> </button>
                     <div className="converter__result">
