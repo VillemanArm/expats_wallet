07763c751b66871154c879b90c6dce61cdd418a3
Villeman <villeman.arm@gmail.com>
2023-11-28 11:17:27 +0300

    feat: add scrollbar gap

--- src/components/History.js
+++ src/components/History.js
@@ -2,6 +2,10 @@ import React from "react";
 import HistoryBlock from "./HistoryBlock";
 import { ImCross } from "react-icons/im";
 
+// TODO: реализовать добавление строк истории в реальном времени
+// TODO: проверить работу отступов при добавлении скроллбара
+// TODO: адаптировать верстку для firefox 
+
 class History extends React.Component {
     constructor(props) {
         super(props);
@@ -9,54 +13,33 @@ class History extends React.Component {
         this.state = {
             isScrollbar: false,
             allData: localStorage.expatsWallet && JSON.parse(localStorage.expatsWallet),
-            //оптимизировать для первой загрузки
         };
 
-        this.addScrollbarGap = this.addScrollbarGap.bind(this)
+        // this.addScrollbarGap = this.addScrollbarGap.bind(this)
         this.delRecord = this.delRecord.bind(this)
         this.clearHistory = this.clearHistory.bind(this)
+        this.checkScrollbar = this.checkScrollbar.bind(this)
+
     }
 
     componentDidMount() {
-        this.addScrollbarGap();
+        this.checkScrollbar()
     }
 
     componentDidUpdate(prevProps, prevState) {
-        localStorage.expatsWallet = JSON.stringify(this.state.allData)
-    }
-
-    //     const targetElement = document.getElementById('yourElementId'); // Замените на ваш реальный идентификатор элемента
-
-    // const resizeObserver = new ResizeObserver(entries => {
-    //   for (const entry of entries) {
-    //     const hasScrollbar = entry.target.scrollHeight > entry.target.clientHeight ||
-    //                          entry.target.scrollWidth > entry.target.clientWidth;
-
-    //     if (hasScrollbar) {
-    //       console.log('Скроллбар появился');
-    //       // Ваш код обработки события
-    //     }
-    //   }
-    // });
-
-    // resizeObserver.observe(targetElement);
-
-    addScrollbarGap() {
-        const historyContainer = document.querySelector(".history__container");
-        const historyClear = document.querySelector(".history__clear");
-        if (historyContainer.scrollHeight > historyContainer.offsetHeight) {
-            historyContainer.style = "padding-right: 1rem";
-            historyClear.style = "margin-right: 1.8rem";
+        if (localStorage.expatsWallet) {
+            localStorage.expatsWallet = JSON.stringify(prevState.allData)
         }
+        console.log(this.state)
     }
 
-    delRecord(id) {
+    async delRecord(id) {
         let newAllData = this.state.allData;
         newAllData.history = this.state.allData.history.filter(
             (record) => record.id !== id
         );
-        this.setState({ allData: newAllData });
-
+        await this.setState({ allData: newAllData });
+        await this.checkScrollbar()
     }
 
     clearHistory() {
@@ -65,19 +48,34 @@ class History extends React.Component {
         this.setState({ allData: newAllData });
     }
 
+    async checkScrollbar() {
+        const converterCurrencies = document.querySelector('.history__container')
+        if (converterCurrencies.scrollHeight > converterCurrencies.offsetHeight) {
+            await this.setState({ isScrollbar: true })
+        } else {
+            await this.setState({ isScrollbar: false })
+        }
+    }
+
     render() {
         return (
             <div className="history">
                 <div className="history__head">
                     <h2>History</h2>
-                    {localStorage.expatsWallet && <button className="history__clear"
-                        onClick={this.clearHistory}
-                    >
-                        <ImCross />
-                    </button>
+                    {(this.state.allData && this.state.allData.history.length > 1) &&
+                        <button
+                            className="history__clear"
+                            style={this.state.isScrollbar ? { marginRight: '1.8rem' } : { marginRight: '0' }}
+                            onClick={this.clearHistory}
+                        >
+                            <ImCross />
+                        </button>
                     }
                 </div>
-                <div className="history__container">
+                <div
+                    className="history__container"
+                    style={this.state.isScrollbar ? { paddingRight: '1rem' } : { paddingRight: '0' }}
+                >
                     {localStorage.expatsWallet && this.state.allData.history.map((record) => (
                         <HistoryBlock
                             key={record.id}
@@ -86,7 +84,7 @@ class History extends React.Component {
                         />
                     ))}
                 </div>
-            </div>
+            </div >
         );
     }
 }
