73ba586c596119fa8e0c7eccea569c580039c233
Villeman <villeman.arm@gmail.com>
2023-12-01 07:46:02 +0300

    feat: add footer block

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,28 @@
 import React from "react";
+import Logo from "./Logo";
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div></div>
-        )
-    }
-}
+const Header = () => {
+    return (
+        <footer>
+            <div className="container footer">
+                <Logo />
+                <div className="footer__description">
+                    <p>
+                        This is an educational project, but if it's convenient
+                        for you to use it, I can transfer it to a separate
+                        domain.
+                    </p>
+                    <p>
+                        You can send suggestions and wishes to my email{" "}
+                        <a href="mailto:mishchenko.maksim.1988@gmail.com?subject=Expat's wallet wishes">
+                            mishchenko.maksim.1988@gmail.com
+                        </a>
+                        .
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Header
+export default Header;
